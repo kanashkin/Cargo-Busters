df43c3ad9dc135f768bbdc34c5a66359ba23b127
Alexander <kanashkin_ad@mail.ru>
2023-11-15 22:03:49 +0300

    add reviews block (slider) to about page

--- js/app.js
+++ js/app.js
@@ -1,56 +1,3 @@
-const menuDropdown = () => {
-    const triggerBtn = document.querySelector('.service-menu-item')
-    const menuItems = document.querySelectorAll('.menu__list-item')
-    const menuBlock = document.querySelector('.submenu')
-
-    triggerBtn.addEventListener('click', e => {
-        e.preventDefault()
-        triggerBtn.classList.toggle('active')
-        menuBlock.classList.toggle('active')
-    })
-}
-
-menuDropdown()
-
-const calc = () => {
-    const reverseAddress = () => {
-        const triggerBtn = document.querySelector('.arrow__block')
-        const fromInput = document.querySelector('.input-from')
-        const toInput = document.querySelector('.input-to')
-
-        triggerBtn.addEventListener('click', () => {
-            let fromValue = fromInput.value
-            let toValue = toInput.value
-
-            fromInput.value = toValue
-            toInput.value = fromValue
-        })
-    }
-
-    const changeMoversAmount = () => {
-        const amountInput = document.querySelector('.movers-input')
-        const minusBtn = document.querySelector('.calc-minus')
-        const plusBtn = document.querySelector('.calc-plus')
-
-        minusBtn.addEventListener('click', () => {
-            if (amountInput.value > 0) {
-                amountInput.value --
-            }
-        })
-
-        plusBtn.addEventListener('click', () => {
-            if (amountInput.value < 20) {
-                amountInput.value ++
-            }
-        })
-    }
-
-    reverseAddress()
-    changeMoversAmount()
-}
-
-calc()
-
 const stockSwiper = new Swiper('.stock-swiper', {
     spaceBetween: 10,
     loop: true,
@@ -115,3 +62,56 @@ const collapse = () => {
 }
 
 collapse()
+
+const menuDropdown = () => {
+    const triggerBtn = document.querySelector('.service-menu-item')
+    const menuItems = document.querySelectorAll('.menu__list-item')
+    const menuBlock = document.querySelector('.submenu')
+
+    triggerBtn.addEventListener('click', e => {
+        e.preventDefault()
+        triggerBtn.classList.toggle('active')
+        menuBlock.classList.toggle('active')
+    })
+}
+
+menuDropdown()
+
+const calc = () => {
+    const reverseAddress = () => {
+        const triggerBtn = document.querySelector('.arrow__block')
+        const fromInput = document.querySelector('.input-from')
+        const toInput = document.querySelector('.input-to')
+
+        triggerBtn.addEventListener('click', () => {
+            let fromValue = fromInput.value
+            let toValue = toInput.value
+
+            fromInput.value = toValue
+            toInput.value = fromValue
+        })
+    }
+
+    const changeMoversAmount = () => {
+        const amountInput = document.querySelector('.movers-input')
+        const minusBtn = document.querySelector('.calc-minus')
+        const plusBtn = document.querySelector('.calc-plus')
+
+        minusBtn.addEventListener('click', () => {
+            if (amountInput.value > 0) {
+                amountInput.value --
+            }
+        })
+
+        plusBtn.addEventListener('click', () => {
+            if (amountInput.value < 20) {
+                amountInput.value ++
+            }
+        })
+    }
+
+    reverseAddress()
+    changeMoversAmount()
+}
+
+calc()
