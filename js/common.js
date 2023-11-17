e0687ce61e1b6d71175a0f1ce8a061089019cd99
Alexander <kanashkin_ad@mail.ru>
2023-11-17 14:38:51 +0300

    add functions to calculator

--- js/common.js
+++ js/common.js
@@ -37,3 +37,62 @@ try {
         },
     })
 } catch {}
+
+try {
+    $(function() {
+        $('.data-input').daterangepicker({
+            singleDatePicker: true,
+            locale: {
+                applyLabel: 'Применить',
+                cancelLabel: 'Отмена',
+                fromLabel: 'От',
+                toLabel: 'До',
+                daysOfWeek: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+                monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            }
+        }, function(start, end, label) {
+            $('.data-value').val(start.format('DD.MM.YYYY'));
+        }
+        );
+    })
+
+
+    const selectOptions = () => {
+        const openTrigger = document.querySelector('.option-input')
+        const menuList = document.querySelector('.options__list')
+        const menuListItems = document.querySelectorAll('.options__list-item')
+        const optionsField = document.querySelector('.options-field')
+
+        openTrigger.onclick = function() {
+            menuList.classList.add('active')
+        }
+
+        window.onclick = function(e) {
+            if (menuList.classList.contains('active')) {
+                if (!e.target.closest('.option-input')) {
+                    menuList.classList.remove('active')
+                }
+            }
+        }
+
+        menuListItems.forEach(item => {
+            item.onclick = function() {
+                item.classList.toggle('selected')
+                
+                let selectedItems = document.querySelectorAll('.options__list-item.selected')
+
+                if (selectedItems.length === 0) {
+                    optionsField.value = 'Без дополнительных опций'
+                } else {
+                    const itemsArr = []
+                    selectedItems.forEach(item => {
+                        itemsArr.push(item.textContent)
+                    })
+                    optionsField.value = itemsArr.join()
+                }
+            }
+        })
+    }
+
+    selectOptions()
+} catch {}
