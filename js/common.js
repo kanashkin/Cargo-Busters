e2ccf18504be5cd62dbbae5439dcc54438386b9f
Alexander <kanashkin_ad@mail.ru>
2023-11-17 20:03:38 +0300

    add new services blocks on index page

--- js/common.js
+++ js/common.js
@@ -38,8 +38,8 @@ try {
     })
 } catch {}
 
-try {
-    $(function() {
+$(function() {
+    try {
         $('.data-input').daterangepicker({
             singleDatePicker: true,
             locale: {
@@ -54,84 +54,85 @@ try {
             $('.data-value').val(start.format('DD.MM.YYYY'));
         }
         );
-    })
+    } catch {}
+})
 
 
-    const selectOptions = () => {
-        const openTrigger = document.querySelector('.option-input')
-        const menuList = document.querySelector('.options__list')
-        const menuListItems = document.querySelectorAll('.options__list-item')
-        const optionsField = document.querySelector('.options-field')
+const selectOptions = () => {
+    const openTrigger = document.querySelector('.option-input')
+    const menuList = document.querySelector('.options__list')
+    const menuListItems = document.querySelectorAll('.options__list-item')
+    const optionsField = document.querySelector('.options-field')
 
-        openTrigger.onclick = function() {
-            menuList.classList.add('active')
-        }
+    openTrigger.onclick = function() {
+        menuList.classList.add('active')
+    }
 
-        window.onclick = function(e) {
-            if (menuList.classList.contains('active')) {
-                if (!e.target.closest('.option-input')) {
-                    menuList.classList.remove('active')
-                }
+    window.onclick = function(e) {
+        if (menuList.classList.contains('active')) {
+            if (!e.target.closest('.option-input')) {
+                menuList.classList.remove('active')
             }
         }
+    }
 
-        menuListItems.forEach(item => {
-            item.onclick = function() {
-                item.classList.toggle('selected')
-                
-                let selectedItems = document.querySelectorAll('.options__list-item.selected')
+    menuListItems.forEach(item => {
+        item.onclick = function() {
+            item.classList.toggle('selected')
+            
+            let selectedItems = document.querySelectorAll('.options__list-item.selected')
 
-                if (selectedItems.length === 0) {
-                    optionsField.value = 'Без дополнительных опций'
-                } else {
-                    const itemsArr = []
-                    selectedItems.forEach(item => {
-                        itemsArr.push(item.textContent)
-                    })
-                    optionsField.value = itemsArr.join()
-                }
+            if (selectedItems.length === 0) {
+                optionsField.value = 'Без дополнительных опций'
+            } else {
+                const itemsArr = []
+                selectedItems.forEach(item => {
+                    itemsArr.push(item.textContent)
+                })
+                optionsField.value = itemsArr.join()
+            }
+        }
+    })
+}
+
+const calc = () => {
+    const reverseAddress = () => {
+        const triggerBtn = document.querySelector('.arrow__block')
+        const fromInput = document.querySelector('.input-from')
+        const toInput = document.querySelector('.input-to')
+
+        triggerBtn.addEventListener('click', () => {
+            let fromValue = fromInput.value
+            let toValue = toInput.value
+
+            fromInput.value = toValue
+            toInput.value = fromValue
+        })
+    }
+
+    const changeMoversAmount = () => {
+        const amountInput = document.querySelector('.movers-input')
+        const minusBtn = document.querySelector('.calc-minus')
+        const plusBtn = document.querySelector('.calc-plus')
+
+        minusBtn.addEventListener('click', () => {
+            if (amountInput.value > 0) {
+                amountInput.value --
+            }
+        })
+
+        plusBtn.addEventListener('click', () => {
+            if (amountInput.value < 20) {
+                amountInput.value ++
             }
         })
     }
 
-    const calc = () => {
-        const reverseAddress = () => {
-            const triggerBtn = document.querySelector('.arrow__block')
-            const fromInput = document.querySelector('.input-from')
-            const toInput = document.querySelector('.input-to')
-    
-            triggerBtn.addEventListener('click', () => {
-                let fromValue = fromInput.value
-                let toValue = toInput.value
-    
-                fromInput.value = toValue
-                toInput.value = fromValue
-            })
-        }
-    
-        const changeMoversAmount = () => {
-            const amountInput = document.querySelector('.movers-input')
-            const minusBtn = document.querySelector('.calc-minus')
-            const plusBtn = document.querySelector('.calc-plus')
-    
-            minusBtn.addEventListener('click', () => {
-                if (amountInput.value > 0) {
-                    amountInput.value --
-                }
-            })
-    
-            plusBtn.addEventListener('click', () => {
-                if (amountInput.value < 20) {
-                    amountInput.value ++
-                }
-            })
-        }
-    
-        reverseAddress()
-        changeMoversAmount()
-    }
-    
-    calc()
+    reverseAddress()
+    changeMoversAmount()
+}
 
+try {
+    calc()
     selectOptions()
 } catch {}
